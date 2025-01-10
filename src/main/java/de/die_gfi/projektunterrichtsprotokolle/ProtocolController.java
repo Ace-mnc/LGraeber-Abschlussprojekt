164a27535d6a3f1193d28fbd5e40f7e5ad48077a
Luke-Graves <76165173+Luke-Graves@users.noreply.github.com>
2025-01-10 13:44:20 +0100

    added basic Methods and 2 pictures

--- src/main/java/de/die_gfi/projektunterrichtsprotokolle/ProtocolController.java
+++ src/main/java/de/die_gfi/projektunterrichtsprotokolle/ProtocolController.java
@@ -1,7 +1,10 @@
 package de.die_gfi.projektunterrichtsprotokolle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class ProtocolController {
     @FXML
@@ -45,4 +48,62 @@ public class ProtocolController {
     @FXML
     public Button btnFinish;
 
+    public void refSelected() {
+    }
+
+    public void msn1Selected() {
+    }
+
+    public void msn2Selected() {
+    }
+
+    public void uOrtTyped() {
+    }
+
+    public void dateVonPicked() {
+    }
+
+    public void dateBisPicked() {
+
+    }
+
+    public void onCheckboxFriday() {
+    }
+
+    public void onDateTyped() {
+    }
+
+    public void onTargetUpdated() {
+    }
+
+    public void onFolderSelect() {
+    }
+
+    public void onZipChecked() {
+    }
+
+    public void onOpenChecked() {
+    }
+
+    public void onOpenClicked() {
+    }
+
+    public void onSortChecked() {
+    }
+
+    public void onCopyChecked() {
+    }
+
+    public void onGenerate() {
+    }
+
+    public void onFinish() {
+    }
+
+    public void editRef() {
+    }
+
+    public void editMsn() {
+
+    }
 }
